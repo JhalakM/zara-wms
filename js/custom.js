6262b0b2453ee0ca6ced54eb91fa83db87f7dcc1
JhalakM <jhalak.mehta@viitorcloud.com>
2015-12-28 15:26:50 +0530

    Commit code for Dropdown changes

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 */
 $(window).load(function(){
 	
-	/* Scrollbar */
+	// Scrollbar JavaScript
 	$(".scroll").mCustomScrollbar({
 		axis:"yx",
 		scrollButtons:{enable:true},
@@ -14,43 +14,7 @@ $(window).load(function(){
 
 $(document).ready(function() {
 	
-	/* Custom Select Box */
-	//$('.select-dropdown').fancySelect();
-	var menu = $('#top').find('menu');
-
-	function positionMenuArrow() {
-		var current = menu.find('.current');
-
-		menu.find('.arrow').css('left', current.offset().left + (current.outerWidth() / 2));
-	}
-
-	$(window).on('resize', positionMenuArrow);
-
-	menu.on('click', 'a', function(e) {
-		var el = $(this),
-			href = el.attr('href'),
-			currentSection = $('#main').find('.current');
-
-		e.preventDefault();
-
-		menu.find('.current').removeClass('current');
-
-		el.addClass('current');
-
-		positionMenuArrow();
-
-		if (currentSection.length) {
-			currentSection.fadeOut(300).promise().done(function() {
-				$(href).addClass('current').fadeIn(300);
-			});
-		} else {
-			$(href).addClass('current').fadeIn(300);
-		}
-	});
-
-	menu.find('a:first').trigger('click')
-	
-	/* Switch */
+	/* Switch JavaScript */
 	$(".toggle-switch a").each(function(){
 		$(this).click(function(){
 			$(this).parents(".toggle-switch").children("li").removeClass("switch");
@@ -58,10 +22,12 @@ $(document).ready(function() {
 		});
 	});
 	
-	
+	// Custom dropdown JavaScript
 	custom_dropdown_list();
 });
 
+
+// function to generate custom dropdown
 function custom_dropdown_list()
 {
 	$('.dropdownlist .selected-listitem').click(function(){
@@ -72,7 +38,6 @@ function custom_dropdown_list()
 	$('.dropdownlist ul li').click(function(){
 		var selectbox_val  = $(this).attr('rel');
 		var selectbox_text = $(this).text();
-		alert(selectbox_val);
 		$(this).parents('.dropdownlist').find('.selected-listitem').text(selectbox_text);
 		$(this).parents('.dropdownlist').find('.dropdown-item').attr('value',selectbox_val);
 		$('.dropdownlist ul').hide();
